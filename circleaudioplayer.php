3b0f567315cbd0367110b7d45ceeea1fafb5ec10
shaif-uddin <shaif_uddin@live.com>
2019-10-13 09:05:44 +0600

    Changing Header file description

--- circleaudioplayer.php
+++ circleaudioplayer.php
@@ -1,22 +1,21 @@
 <?php
 /*
 Plugin Name: Circle Audio Player
-Plugin URI: http://tech.sugrihini.com
+Plugin URI: https://github.com/shaif-uddin/circleaudioplayer
 Description: Play Circle Audio Player for WordPress
 Version: 1.0
 Author: Shaif Uddin Ahamed
 Author URI: http://tech.sugrihini.com
 License: GPL2
+License URI: http://www.gnu.org/licenses/gpl-2.0.txt
 Text Domain: circleaudioplayer
 Domain Path: /languages
 */
 add_action('wp_enqueue_scripts','circleaudioplayer_assets');
 function circleaudioplayer_assets(){
-    wp_enqueue_style( 'bar', plugins_url( '/progres-bar.css' , __FILE__ ));
-	//wp_enqueue_script('jquery');
-    wp_enqueue_script( 'player', plugins_url( '/player.js' , __FILE__ ),array("jquery"),'0.0.1',false);
-    //wp_enqueue_script( 'main', plugins_url( '/main.js' , __FILE__ ),array("jquery","player"),'0.0.3',false);
-	wp_enqueue_script( 'main', plugins_url( '/main.js' , __FILE__ ), array("jquery","player") ,'1.0.2',false);
+    wp_enqueue_style( 'progres-bar', plugins_url( '/progres-bar.css' , __FILE__ ));
+	wp_enqueue_script( 'player', plugins_url( '/player.js' , __FILE__ ),array("jquery"),'0.0.1',false);
+    wp_enqueue_script( 'main', plugins_url( '/main.js' , __FILE__ ), array("jquery","player") ,'1.0.2',false);
 
 }
 
